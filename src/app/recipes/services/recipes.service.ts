7f9b51849cb3053de2890ed5e3e811163aefab0b
Jadir Junior <jadirjsjunior@gmail.com>
2024-05-22 17:55:36 -0300

    remove unnecessary import

--- src/app/recipes/services/recipes.service.ts
+++ src/app/recipes/services/recipes.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 const BASE_PATH = environment.BASE_PATH;
