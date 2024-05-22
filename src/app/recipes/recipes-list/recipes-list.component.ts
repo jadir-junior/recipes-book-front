c4342508ba569eb10e6e1f0863fac830c3fe9d22
Jadir Junior <jadirjsjunior@gmail.com>
2024-05-22 18:48:16 -0300

    create template to cards in recipes-list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RecipesService } from '../services/recipes.service';
 import { DataViewModule } from 'primeng/dataview';
+import { RatingModule } from 'primeng/rating';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-recipes-list',
   standalone: true,
-  imports: [DataViewModule],
+  imports: [
+    DataViewModule,
+    RatingModule,
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     @if(recipes$ | async; as recipes) {
     <p-dataView
@@ -15,10 +26,39 @@ import { DataViewModule } from 'primeng/dataview';
       [rows]="9"
       filterBy="name"
       layout="grid"
-    ></p-dataView>
+    >
+      <ng-template let-recipe pTemplate="gridItem">
+        <div class="p-col-12 p-md-3">
+          <div>
+            <div>
+              <div>
+                <i class="pi pi-tag"></i>
+                <span>{{ recipe.category }}</span>
+              </div>
+            </div>
+            <div>
+              <img
+                [src]="'assets/recipes/' + recipe.imageUrl"
+                [alt]="recipe.title"
+              />
+              <div>{{ recipe?.title }}</div>
+              <div>{{ recipe?.title }}</div>
+              <p-rating
+                [ngModel]="recipe.rating"
+                [readonly]="true"
+                [cancel]="false"
+              ></p-rating>
+            </div>
+            <div>
+              <span>{{ recipe?.prepTime }} min</span>
+              <p-button icon="pi pi-heart"></p-button>
+            </div>
+          </div>
+        </div>
+      </ng-template>
+    </p-dataView>
     }
   `,
-  styles: ``,
 })
 export class RecipesListComponent {
   recipes$ = this.recipeService.recipes$;
