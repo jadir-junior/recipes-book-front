6bcd76368ab4e42f6c40a23d9c804aba37f94c15
Jadir Junior <jadirjsjunior@gmail.com>
2024-05-22 21:26:13 -0300

    create route to component recipes list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./recipes/recipes-list/recipes-list.component').then(
+        (comp) => comp.RecipesListComponent
+      ),
+  },
+];
